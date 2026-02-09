aaf7abcd156d2c79296be60aed966aeb09c88978
Joseph-ask <joejoe.a.2005@gmail.com>
2026-02-09 22:22:41 +0000

    check play again function+tidy

--- Task-1/tic-tac-toe.cpp
+++ Task-1/tic-tac-toe.cpp
@@ -16,29 +16,40 @@ std::cout <<
        "     I     I     \n";
 }
 
-int main(){
-
-    printBoard();
-
-    if(play==true){
-        //play tha game
-    }
-    
+bool checkPlayAgain(){
     bool validInput{false};
     char input{};
+    bool p{true};
     while (validInput == false && played == true){
         std::cout << "Would you like to play again? (y/n) " ;
         std::cin >> input;
         if(input!='y' && input!='n'){
-            std::cout << "Invalid input, y/n only";
+            std::cout << "Invalid input, y/n only \n";
         }
-        else if(input == 'n'){
+        else if(input == 'y'){
             validInput == true;
+            p=true;
         }
         else{
-          play=false;
-          validInput=true;
+            validInput=true;
+            p=false;
         } 
     } 
+    return p;
+}
+
+
+
+int main(){
+
+    printBoard();
+
+    while(play==true){
+        //play tha game
+
+    played = true;
+    
+    play = checkPlayAgain();
+    }
     std::cout <<"Thank you for playing!!";
 }
