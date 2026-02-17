fdfb97779ba84d238443ea526467c58c12c790a6
Joseph-ask <joejoe.a.2005@gmail.com>
2026-02-17 18:48:12 +0000

    Uhhh

--- Task-1/tic-tac-toe.cpp
+++ Task-1/tic-tac-toe.cpp
@@ -15,6 +15,23 @@ std::cout <<
        "     I     I     \n";
 }
 
+void playerMove(char b[2][2]){
+    int move{0};
+    while(true){
+        std::cout << "Enter your move, using numeric keyboard method.";
+        std::cin >> move;
+        int row = 2-(move-1)/3;
+        int col=(move-1)%3;
+        if (b[row][col]!= ' . '){
+            std::cout << "Invalid move, try again";
+        }
+        else{
+            b[row][col]=' x ';
+            break;
+        }
+    }
+}
+
 bool checkPlayAgain(){
     bool validInput{false};
     char input{};
@@ -36,10 +53,10 @@ bool checkPlayAgain(){
 bool checkWinCons(int a[8]){
     for(int i=0; i<3; i++){
         if(a[i]==a[i+3] && a[i]==a[i+6]){return true;}
-        if(a[i*3]==a[i*3+1] && a[i*3]==a[i*3+2]){return true};
+        if(a[i*3]==a[i*3+1] && a[i*3]==a[i*3+2]){return true;}
     }
-    if(a[0]==a[4] && a[0]==a[8]){return true};
-    if(a[2]==a[4] && a[2]==a[6]){return true};
+    if(a[0]==a[4] && a[0]==a[8]){return true;}
+    if(a[2]==a[4] && a[2]==a[6]){return true;}
     return false;
 }
 
