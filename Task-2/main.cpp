cdaf4e43f4e71ee4152bfa6e5eaf6a78a1f28025
Alasdair Morrison <2742064m@student.gla.ac.uk>
2026-03-27 14:23:31 +0000

    adjusted to use single dataset

--- Task-2/main.cpp
+++ Task-2/main.cpp
@@ -68,22 +68,22 @@ void test_gradient_regression() {
     GradientRegression model(learningRate, numEpochs, "data.csv");
 
     gradient_predictions.clear();
-    model.predict(gradient_dataset);
-    const double initial_loss = model.compute_loss(gradient_predictions, gradient_dataset);
+    model.predict(dataset);
+    const double initial_loss = model.compute_loss(gradient_predictions, dataset);
 
     for (int epoch = 0; epoch < numEpochs; ++epoch) {
         gradient_predictions.clear();
-        model.predict(gradient_dataset);
+        model.predict(dataset);
         model.update_parameters();
     }
 
     gradient_predictions.clear();
-    model.predict(gradient_dataset);
-    const double final_loss = model.compute_loss(gradient_predictions, gradient_dataset);
+    model.predict(dataset);
+    const double final_loss = model.compute_loss(gradient_predictions, dataset);
 
     std::cout << "Initial Loss: " << initial_loss << std::endl;
     std::cout << "Final Loss: " << final_loss << std::endl;
-    if (!gradient_dataset.empty() && final_loss < initial_loss) {
+    if (!dataset.empty() && final_loss < initial_loss) {
         std::cout << "Test Passed: Loss decreased." << std::endl;
     } else {
         std::cerr << "Test Failed: Loss did not decrease." << std::endl;
@@ -102,15 +102,18 @@ void test_multi_regression() {
 
     MultiGradientRegression model(learningRate, numEpochs, "dataMulti.csv");
 
-    gradient_predictions_multi = model.predict(gradient_dataset_multi);
+    gradient_predictions_multi.clear();
+    model.predict(gradient_dataset_multi);
     const double initial_loss = model.compute_loss(gradient_predictions_multi, gradient_dataset_multi);
 
     for (int epoch = 0; epoch < numEpochs; ++epoch) {
-        gradient_predictions_multi = model.predict(gradient_dataset_multi);
+        gradient_predictions_multi.clear();
+        model.predict(gradient_dataset_multi);
         model.update__multi_parameters();
     }
 
-    gradient_predictions_multi = model.predict(gradient_dataset_multi);
+    gradient_predictions_multi.clear();
+    model.predict(gradient_dataset_multi);
     const double final_loss = model.compute_loss(gradient_predictions_multi, gradient_dataset_multi);
 
     std::cout << "Initial Loss: " << initial_loss << std::endl;
