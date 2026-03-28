aa9088982bfee9299152b55311139a6fa70884eb
Joseph-ask <joejoe.a.2005@gmail.com>
2026-03-28 16:54:02 +0000

    Added comments to MDG

--- Task-2/multiDataGen.cpp
+++ Task-2/multiDataGen.cpp
@@ -4,24 +4,25 @@
 #include <random>
 #include <fstream>
 
-void generateMultiDataset(int num_points, double trueOmega1, double trueOmega2, double trueBeta, double noise_level) {
-    std::mt19937 gen(42); 
-    
-    std::uniform_real_distribution<double> uniform(0.0, 1.0);
-    
-    std::normal_distribution<double> normal(0.0, noise_level);
+//Function to generate a CSV file with two X-coords and one Y-coord for multi-linear regression formula
 
+void generateMultiDataset(int num_points, double trueOmega1, double trueOmega2, double trueBeta, double noise_level) {
+    //Sets up random number gen and distributors to create data points
+    std::mt19937 gen(42); 
+    std::uniform_real_distribution<double> uniform(0.0, 1.0);
+    std::normal_distribution<double> normal(0.0, noise_level);
+    //initiallises vectors for storing data points
     std::vector<double> X1, X2, y;
 
+    //creates number of data points specified by num_points
     for (int i = 0; i < num_points; ++i) {
         double x1_val = 2.0 * uniform(gen);
         X1.push_back(x1_val);
 
         double x2_val = 4.0 * uniform(gen);  //Second X-point gen
-        //may need to alter to make different from first xval
         X2.push_back(x2_val);
 
-
+        //creates Y-val based of function parameters and noise
         double noise = 0.25 * normal(gen);
         double y_val = trueBeta + trueOmega2 * x2_val + trueOmega1 * x1_val + noise;
         y.push_back(y_val);
@@ -29,6 +30,7 @@ void generateMultiDataset(int num_points, double trueOmega1, double trueOmega2, 
 
     }
 
+    //Writes each data point to CSV file and adds header of x1,x2,y
     std::ofstream outFile("dataMulti.csv");
     outFile << "X1,X2,y\n"; // Header
     for (size_t i = 0; i < X1.size(); ++i) {
