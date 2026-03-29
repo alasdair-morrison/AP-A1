a97787114ddea93167480358794e4a14cb6ff34d
Joseph-ask <joejoe.a.2005@gmail.com>
2026-03-29 13:31:42 +0100

    Finished comments on tictactoe

--- Task-1/tic-tac-toe.cpp
+++ Task-1/tic-tac-toe.cpp
@@ -70,6 +70,7 @@ void AIMove(char b[3][3], char p) {
     }
 }
 
+//Resets 3x3 board array if player wants to play again
 void resetboard(char a[3][3]){
     for(int i = 0; i<3; i++){
         for(int j = 0; j<3; j++){
@@ -78,18 +79,24 @@ void resetboard(char a[3][3]){
     }
 }
 
+//checks if player wants to play again
 bool checkPlayAgain(){
     char input{};
     while (true){
         std::cout << "Would you like to play again? (y/n) " ;
         std::cin >> input;
+
+        //makes sure user has entered a valid character
         if(input!='y' && input!='n'){
             std::cout << "Invalid input, y/n only \n";
         }
+
+        //if user wants to play again, reset board and return true
         else if(input == 'y'){
             resetboard(board);
             return true;
         }
+
         else{
             return false;
         } 
@@ -99,11 +106,11 @@ bool checkPlayAgain(){
 
 bool checkWinCons(char a[3][3], char p){
     for(int i=0; i<3; i++){
-        //checking straights for winners
+        //checking straights for winners (three of either y or x)
         if(a[i][0]==p && a[i][1]==p && a[i][2]==p){return true;}
         if(a[0][i]==p && a[1][i]==p && a[2][i]==p){return true;}
     }
-    //check diagonals for winners
+    //check diagonals for winners (three of either y or x)
     if(a[0][0]==p && a[1][1]==p && a[2][2]==p){return true;}
     if(a[0][2]==p && a[1][1]==p && a[2][0]==p){return true;}
     return false;
@@ -112,10 +119,12 @@ bool checkWinCons(char a[3][3], char p){
 int main(){
 
     while(play==true){
+        //initialise starter values
         int i = 0;
         bool winnercheck = false;
         char playerturn='x';
 
+        //checks what mode the player would like to play
         int input{3};
         while(input != 2 && input != 1){
             std::cout << "Play AI or Person (1/2)? " ;
@@ -123,15 +132,21 @@ int main(){
         }
 
         if(input==1){
+            //while the board is not full and noone has reached three in a row
             while(i<9 && winnercheck == false){
                 if(i%2==0){
+                    //set the turn to x and allow the player to choose a square
                     playerturn='x';
                     playerMove(board,playerturn);
                 }
+
+                //sets the turn to "o" and chooses random square
                 else{
                     playerturn='o';
                     AIMove(board,playerturn);
                 }
+
+                //prints current board after the current turn is over and checks for winners
                 printBoard(board);
                 winnercheck = checkWinCons(board,playerturn);
                 i++;
@@ -139,21 +154,29 @@ int main(){
         }
 
         if(input==2){
+            //while the board is not full and noone has reached three in a row
             while(i<9 && winnercheck == false){
+                //switches between naughts and crosses every turn
                 if(i%2==0){playerturn='x';}
                 else{playerturn='o';}
+
+                //allows current player to choose square and prints board
                 playerMove(board,playerturn);
                 printBoard(board);
+
+                //checks for a winner
                 winnercheck = checkWinCons(board,playerturn);
                 i++;
             }
         }  
 
-        //Print win situation
+        //Print win situation e.g."x's has won" or "match waws a draw"
         if (winnercheck == true){std::cout<< playerturn << " has won!\n";}
         else{std::cout<<"The match was a draw!\n";}
 
+        //checks if the player wants to play again
         play = checkPlayAgain();
     }
+    //friendly message before closing program (:
     std::cout <<"Thank you for playing!!";
 }
